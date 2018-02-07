c16e3c8c7730d3c4f267bc16526c33a84294c4c3
スペンサー <spencerpomme@live.com>
2018-02-07 15:42:51 +0800

    Intended collection of sorting algorithms

---
+++ algorithms/sort/Comparison.py
@@ -0,0 +1,39 @@
+# Insertion sort algorithm
+# from collections import Iterable
+
+
+def insertion_sort():
+    """
+    Basic insertion sort algorithm that can be applied on linear data structure
+    """
+    print("insertion sort test!")
+
+
+def quick_sort():
+    print("quick sort test!")
+
+
+def merge_sort():
+    print("merge sort test!")
+
+
+def inplace_merge_sort():
+    print("inplace merge sort test!")
+
+
+def heap_sort():
+    print("heap sort test!")
+
+
+def selection_sort():
+    print("selection sort test!")
+
+
+def bubble_sort():
+    print("bubble sort test!")
+
+
+def tree_sort():
+    print("tree sort test!")
+
+
---
+++ tests/sort_test.py
@@ -0,0 +1,5 @@
+from algorithms.sort import Comparison
+
+
+if __name__ == "__main__":
+    Comparison.bubble_sort()
